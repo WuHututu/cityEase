a07240601a0e5109be200072f9d3e93cdd4aa9ef
WuHututu <909244081@qq.com>
2026-01-27 13:55:37 +0800

    first

---
+++ cityEase-web/src/main/java/nynu/cityEase/web/Application.java
@@ -0,0 +1,19 @@
+package nynu.cityEase.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 90924
+ * Date: 2026/1/27
+ * Time: 13:23
+ * Description: TODO
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
